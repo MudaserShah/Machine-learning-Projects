27e26d51f9f4784701afaf51687023311e0a5d9a
Syed Mudaser Shah <syedmudaser92@gmail.com>
2026-04-27 21:36:06 +0500

    add updated files

--- Laptop-price-preciton/app.py
+++ Laptop-price-preciton/app.py
@@ -6,7 +6,7 @@ import pandas as pd
 pipe = pickle.load(open('pipe.pkl','rb'))
 df = pickle.load(open('df.pkl','rb'))
 
-st.title("Laptop Predictor")
+st.title("Laptop Price Predictor")
 
 # Inputs
 company = st.selectbox('Brand', df['Company'].unique())
